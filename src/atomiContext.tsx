a85644fa8a285586f3abf544456f513f735ff116
tommyrharper <thomasrobertharper@gmail.com>
2021-05-15 17:49:03 +0100

    Create atomiContext Provider

---
+++ src/atomiContext.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const AppContext = React.createContext({
+  url: ''
+});
+
+type MyProps = { children: React.ReactNode; url: string; };
+type MyState = { url: string };
+
+export class AtomiProvider extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props)
+    this.state = {
+      url: props.url
+    }
+  }
+
+  render() {
+    const { url } = this.state;
+    return (
+      <AppContext.Provider value={{url}}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
