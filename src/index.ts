0a475ebc5ab288a82af143124d2949c6ad76259c
tommyrharper <thomasrobertharper@gmail.com>
2021-05-18 04:03:35 +0100

    Create index.ts
    Co-authored-by: Xiao <xiaotongli@users.noreply.github.com>
    Co-authored-by: Paulo Choi <paulochoi@users.noreply.github.com>
    Co-authored-by: Pat Liu <patrickliuhhs@users.noreply.github.com>

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import AtomiProvider from './atomiContext';
+import useQuery from './useQuery';
+
+export {
+  AtomiProvider,
+  useQuery
+};
+
+// export default AtomiProvider;
+// export default useQuery;
