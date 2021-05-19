9978ecf89ae0babf4c9af1b3fd7e52d6c2de6bf3
Paulo Choi <mail@paulochoi.com>
2021-05-18 22:35:44 -0400

    Setup prettier and ESLint, run prettier on source files

--- src/atomiContext.tsx
+++ src/atomiContext.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
 export const AppContext = React.createContext({
-  url: ''
+  url: '',
 });
 
-type MyProps = { children: React.ReactNode; url: string; };
+type MyProps = { children: React.ReactNode; url: string };
 type MyState = { url: string };
 
 export default class AtomiProvider extends React.Component<MyProps, MyState> {
   constructor(props: MyProps) {
-    super(props)
+    super(props);
     this.state = {
-      url: props.url
-    }
+      url: props.url,
+    };
   }
 
   render() {
     const { url } = this.state;
     return (
-      <AppContext.Provider value={{url}}>
+      <AppContext.Provider value={{ url }}>
         {this.props.children}
       </AppContext.Provider>
     );
