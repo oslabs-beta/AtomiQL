2dbcb17aac241f4f9db6af4a0e28194e4b83cd75
Xiao Li <lixtong@gmail.com>
2021-05-22 15:50:57 -0400

    Added writing and reading add to cache in store > > Co-authored-by: patrickliuhhs <patrickliu.hhs@gmail.com>

--- src/atomiContext.tsx
+++ src/atomiContext.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 
-export const AppContext = React.createContext({
-  url: '',
-});
+export const AppContext = React.createContext<Partial<MyProps>>({});
 
-type MyProps = { children: React.ReactNode; url: string };
-type MyState = { url: string };
+type MyProps = { children: React.ReactNode; url: string | any; cache?: any; setCache?: any };
+type MyState = { 
+  url: string, 
+  cache?: object,
+  setCache?: any
+};
 
 export default class AtomiProvider extends React.Component<MyProps, MyState> {
   constructor(props: MyProps) {
     super(props);
+    this.setCache = this.setCache.bind(this);
     this.state = {
       url: props.url,
-    };
+      cache: {},
+      setCache: this.setCache
+    }
+  }
+
+  setCache = (query: string, atomData: object) => {
+    this.setState(
+      {
+        cache: {[query]: atomData, ...this.state.cache}
+      }
+    )
   }
 
   render() {
-    const { url } = this.state;
     return (
-      <AppContext.Provider value={{ url }}>
+      <AppContext.Provider value={this.state}> 
         {this.props.children}
       </AppContext.Provider>
     );
