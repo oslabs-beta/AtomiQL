bfea270765edff3b39d858326bfc76f8f776926f
patrickliuhhs <patrickliu.hhs@gmail.com>
2021-05-27 18:49:14 -0400

    idk what i did

--- src/useQuery.ts
+++ src/useQuery.ts
@@ -5,7 +5,7 @@ import { request } from 'graphql-request';
 import { useEffect, useContext, useRef } from 'react';
 import { AppContext } from './atomiContext';
 
-interface AtomData {
+export interface AtomData {
   loading: boolean;
   data: null | { [key: string]: any };
   hasError: boolean;
@@ -19,7 +19,16 @@ const initialAtomData: AtomData = {
   hasError: false,
 };
 
-const newAtom = atom(initialAtomData);
+const newAtom = atom(initialAtomData)
+// const newAsyncAtom = atom(async (get:any, set:any) => set(newAtom, get(newAtom) + 1));
+// const newAsyncAtom = atom(async (get) => get(newAtom));
+// const asyncFetchAtom = atom(
+//   (get) => get(newAtom),
+//   async (_get, set, url) => {
+//     const response = await request(url, query);
+//     set(newAtom, (await response))
+//   }
+// )
 
 const useQuery = (query: string): AtomDataArray => {
 
@@ -28,34 +37,43 @@ const useQuery = (query: string): AtomDataArray => {
   const hasError = useRef(false);
   const data = useRef<{ [key: string]: any; } | null>(null);
 
+  // console.log('cache in usequery', cache);
+  console.log('cache.query in usequery', cache[query]);
+
   const [atomData, setAtom] = useAtom(newAtom);
   loading.current = atomData.loading;
   hasError.current = atomData.hasError;
   data.current = atomData.data;
+
+  console.log('atomData in useQuery', atomData)
+
+  // const [cachedAtomData] = useAtom(cacheResponse);
 
 
   useEffect(() => {
     (async () => {
       try {
         const cacheResponse = cache[query];
+        // console.log('cacheResponse in useeffect', cacheResponse);
         if (cacheResponse) {
-          // console.log('you did it!');
-          loading.current = cache[query].loading;
-          hasError.current = cache[query].hasError;
-          data.current = cache[query].current;
+          console.log('you did it!');
+          const [cachedAtomData] = useAtom(cacheResponse);
+          // console.log('cachedAtomData after cache pull', cachedAtomData)
+
+          loading.current = cachedAtomData.loading;
+          hasError.current = cachedAtomData.hasError;
+          data.current = cachedAtomData.data;
         } else {
           const result = await request(url, query);
           // console.log('RESULT IS',result)
-          setCache(query, {
-            data: result,
-            loading: false,
-            hasError: false
-          });
+
           setAtom({
             data: result,
             loading: false,
             hasError: false,
           });
+
+          setCache(query, newAtom);
         }
       } catch {
         setAtom({
@@ -67,6 +85,11 @@ const useQuery = (query: string): AtomDataArray => {
     })();
     /* eslint react-hooks/exhaustive-deps:0 */
   }, []);
+
+  useEffect(() => {
+    console.log('newAtom in 2nd useeffect', newAtom);
+  }, [newAtom])
+
 
   return [data.current, loading.current, hasError.current];
 };
