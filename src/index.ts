f6dab51d888a85aaa5ea8e8a031455271f2edd29
tommyrharper <thomasrobertharper@gmail.com>
2021-05-30 00:55:08 +0100

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
 import AtomiProvider from './atomiContext';
 import useQuery from './useQuery';
 import useMutation from './useMutation';
+import {
+  ResponseData,
+  AtomData,
+  AtomiAtom,
+  AtomiAtomContainer,
+  ReadQueryOutput,
+  CacheContainer,
+} from './types';
 
-export { AtomiProvider, useQuery, useMutation };
+export {
+  AtomiProvider,
+  useQuery,
+  useMutation,
+  ResponseData,
+  AtomData,
+  AtomiAtom,
+  AtomiAtomContainer,
+  ReadQueryOutput,
+  CacheContainer,
+};
