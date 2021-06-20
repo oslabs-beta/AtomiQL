4752add4e1bab1ed53e497b0cff8133aa5af2258
tommyrharper <thomasrobertharper@gmail.com>
2021-06-20 03:50:48 +0100

    Add comments to useQuery.ts

--- src/useQuery.ts
+++ src/useQuery.ts
@@ -14,17 +14,22 @@ const initialAtomData: AtomData = {
 };
 
 const useQuery = (query: Query, input?: any): AtomDataArray => {
+  // Parse the graphQL query
   const { updatedAST, queryString, pathToResolver, foundClientDirective } =
     parseQuery(query);
+  // Access the cache
   const { cache, setCache, graphQLClient, resolvePathToResolvers, resolvers } =
     useContext(AtomiContext);
+  // Look for a cachedAtom
   const cachedAtom = cache[queryString] ? cache[queryString].atom : null;
+  // If there is no cached atom, set the active atom to be a new atom
   const activeAtom: AtomiAtom = cachedAtom || atom(initialAtomData);
-
+  // Hooke into the activeAtom
   const [atomData, setAtom] = useAtom(activeAtom);
 
   useEffect(() => {
     (async () => {
+      // If the atom is cached do not query the server
       if (!cachedAtom) {
         const newAtomData: AtomData = {
           data: null,
@@ -32,25 +37,33 @@ const useQuery = (query: Query, input?: any): AtomDataArray => {
           hasError: false,
         };
         try {
+          // Query the server
           const result = await graphQLClient.request(updatedAST, input);
+          // If there are @client directives in the query, merge the result from
+          // the server with local state from the resolvers for those Fields
           if (foundClientDirective) {
             resolvePathToResolvers(pathToResolver, resolvers);
             mergeServerAndLocalState(result, pathToResolver);
           }
           newAtomData.data = result;
+          // Set the response in the cache
           setCache(queryString, {
             atom: activeAtom,
             atomData: newAtomData,
             setAtom,
           });
+          // Update the value of the Jotai atom
           setAtom(newAtomData);
         } catch {
+          // Catch any errors
           newAtomData.hasError = true;
+          // Set the cache
           setCache(queryString, {
             atom: activeAtom,
             atomData: newAtomData,
             setAtom,
           });
+          // Update the value of the Jotai atom
           setAtom(newAtomData);
         }
       }
@@ -58,6 +71,7 @@ const useQuery = (query: Query, input?: any): AtomDataArray => {
     /* eslint react-hooks/exhaustive-deps:0 */
   }, []);
 
+  // Return to the user data about and response from their request
   return [atomData.data, atomData.loading, atomData.hasError];
 };
 
