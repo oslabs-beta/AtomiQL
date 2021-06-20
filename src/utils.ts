24c4ecd307bd0b55f8e0ff962ad1337e47bd9335
tommyrharper <thomasrobertharper@gmail.com>
2021-06-20 02:32:42 +0100

    Add comments to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -19,17 +19,22 @@ export const mergeServerAndLocalState = (
   serverState: ServerState,
   pathToResolver: PathObject
 ) => {
+  // If pathToResolver is falsy hit the base case
   if (!pathToResolver) return;
+  // If serverState is an array, recursively call each element and return out
   if (Array.isArray(serverState)) {
-    serverState.forEach((stateEl: any) =>
+    serverState.forEach((stateEl: ServerState) =>
       mergeServerAndLocalState(stateEl, pathToResolver)
     );
     return;
   }
 
+  // Otherwise iterate through each key value pair in the  pathToResolver object
   for (const [pathKey, pathValue] of Object.entries(pathToResolver)) {
+    // If pathToResolver says resolver locally, update the serverState with the local state
     if (resolveLocally(pathValue))
       serverState[pathKey] = pathValue.resolveLocally;
+    // Otherwise recursively call at the next level of depth in the server and path objects
     else mergeServerAndLocalState(serverState[pathKey], pathValue);
   }
 };
